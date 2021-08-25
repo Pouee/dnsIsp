66e426ace08d408d1d0727e7183d8d2b4762aec8
Pouee <pouee@hotmail.com>
2021-08-25 22:24:04 +0800

    优化

--- src/main/java/com/cocofhu/dnsisp/dns/DNSServer.java
+++ src/main/java/com/cocofhu/dnsisp/dns/DNSServer.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DNSServer implements Runnable{
-    public static final int IP = (106<<24) + (55<<16) + (235<<8) + (158);
+    public static final int IP = (119<<24) + (29<<16) + (119<<8) + (98);
     private Map<String,String> mapping;
     public DNSServer(Map<String, String> mapping) {
         this.mapping = mapping;
@@ -16,35 +16,35 @@ public class DNSServer implements Runnable{
     @Override
     public void run() {
 
-        try(
-                DatagramSocket server = new DatagramSocket(53);
-                ) {
-            while(true){
-                byte[] data = new byte[1024];
-                DatagramPacket packet = new DatagramPacket(data,data.length);
-                server.receive(packet);
-                DNSHeader header = new DNSHeader(data);
-                DNSQuestion question = new DNSQuestion(data, 12);
-                DNSAnswer answer = new DNSAnswer(IP);
-                InetAddress address = packet.getAddress();
+        while(true){
+            try(DatagramSocket server = new DatagramSocket(53)) {
+                while(true){
+                    byte[] data = new byte[1024];
+                    DatagramPacket packet = new DatagramPacket(data,data.length);
+                    server.receive(packet);
+                    DNSHeader header = new DNSHeader(data);
+                    DNSQuestion question = new DNSQuestion(data, 12);
+                    DNSAnswer answer = new DNSAnswer(IP);
+                    InetAddress address = packet.getAddress();
 
-                int port = packet.getPort();
-                ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                header.query = 1;
-                header.qdCount = 1;
-                header.anCount = 1;
-                bos.write(header.getBytes());
-                for(int i = 0 ; i < question.getLength() ; ++i) bos.write(data[12+i]);
-                bos.write(answer.getBytes());
-                byte[] bytes = bos.toByteArray();
-                DatagramPacket packet2 = new DatagramPacket(bytes, bytes.length, address, port);
-
-                mapping.put(question.getName(),packet.getAddress().getHostAddress());
-                server.send(packet2);
+                    int port = packet.getPort();
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                    header.query = 1;
+                    header.qdCount = 1;
+                    header.anCount = 1;
+                    bos.write(header.getBytes());
+                    for(int i = 0 ; i < question.getLength() ; ++i) bos.write(data[12+i]);
+                    bos.write(answer.getBytes());
+                    byte[] bytes = bos.toByteArray();
+                    DatagramPacket packet2 = new DatagramPacket(bytes, bytes.length, address, port);
+                    mapping.put(question.getName(),packet.getAddress().getHostAddress());
+                    server.send(packet2);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+
 
 
 
